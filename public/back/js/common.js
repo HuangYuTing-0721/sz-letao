531b4b94f92b94ff87afc3475ce55b305b2ad166
Ting <hyt@qq.com>
2017-11-09 13:17:46 +0800

    首页的基本功能

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -8,3 +8,12 @@ $(document).ajaxStop(function(){
         NProgress.done();
     },500);
 });
+
+//二级菜单显示隐藏
+//prev() 取上一个元素
+//next() 去下一个元素
+//slideToggle() 高度变化，显示是从上到下变换，隐藏是从下到上
+$(".classify").prev().on("click", function () {
+    $(this).next().slideToggle();
+});
+
